3981d914758a186643a7e92f3e4c044b25bbced4
Athan <jonpuray@gmail.com>
2025-06-30 15:35:50 +0800

    added connection to gsheet

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def archive():
                 filtered_df = df[(df['DATE'] == formatted_date_1) & (df['CLIENT NAME'] == cl)]
 
             selected_columns = filtered_df[['DATE', 'TIER', 'LINK']]
-            # st.write(cl)
+            st.write(cl)
             st.write(formatted_date_1)
             st.dataframe(selected_columns, use_container_width=True, hide_index=True)
 
