3706bca7074edc5c777f653957e4aca6a7417453
Athan <jonpuray@gmail.com>
2025-07-03 11:47:57 +0800

    updated

--- main.py
+++ main.py
@@ -90,6 +90,9 @@ def archive():
                         selected_columns = filtered_df[['DATE', 'TIER', 'LINK']]
                         st.header(cl)
                         st.dataframe(selected_columns, use_container_width=True, hide_index=True)
+                    elif filtered_df.shape[0] == 0:
+                        st.error('No Data Found')
+
             elif _client == []:
                 st.error('Select a client from the list')
 
