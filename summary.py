d83134c311b6109c0c086ce6f764ab377c8c3945
Athan <jonpuray@gmail.com>
2025-07-12 11:23:31 +0800

    updated

--- summary.py
+++ summary.py
@@ -42,7 +42,6 @@ def summary(client):
                 filtered_df = df[df['CLIENT NAME'].isin(c_list)]
                 count_df = filtered_df['CLIENT NAME'].value_counts()
 
-            count_df.rename({'COUNT':'count'}, inplace=True)
             st.dataframe(count_df)           
     
     with col2:
