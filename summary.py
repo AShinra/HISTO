f49875a1494abc1384a4c491aff9ad401b5d2729
Athan <jonpuray@gmail.com>
2025-07-12 10:46:53 +0800

    updated

--- summary.py
+++ summary.py
@@ -38,7 +38,7 @@ def summary(client):
         if button_select:
             # filtered_df = df[(df['CLIENT NAME'] in c_list)]
             filtered_df = df['CLIENT NAME'].isin(c_list)
-            st.dataframe(filtered_df)
+            st.dataframe(df['CLIENT NAME'].value_counts())
             st.write(c_list)
     
     with col2:
