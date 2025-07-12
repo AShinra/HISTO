5a0445e45f9fd0d4b5314dab819423d127306eab
Athan <jonpuray@gmail.com>
2025-07-12 10:36:35 +0800

    updated

--- summary.py
+++ summary.py
@@ -32,7 +32,8 @@ def summary(client):
 
     with col1:
         st.header('Client Misses')
-        st.dataframe(df['CLIENT NAME'].value_counts(sort=False))
+        client_df = df['CLIENT NAME'].value_counts()
+        st.multiselect('Select Client', options=client_list)
     
     with col2:
         st.header('Missed per Date')
