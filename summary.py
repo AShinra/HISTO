20e890626e0630a20ddda24f7f3bffe984673c53
Athan <jonpuray@gmail.com>
2025-07-15 12:33:59 +0800

    updated

--- summary.py
+++ summary.py
@@ -40,7 +40,7 @@ def summary(client):
     with cola:
         st.dataframe(count_month)
     with colb:
-        st._chart(count_month, use_container_width=True, x=df['MONTH_NAME'])
+        st._chart(count_month, use_container_width=True, x=df.columns[0])
 
     col1, col2 = st.columns(2, border=True)
 
