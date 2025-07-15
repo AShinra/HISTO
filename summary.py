00db86285ab526dbe651140731062c88874c8518
Athan <jonpuray@gmail.com>
2025-07-15 14:27:39 +0800

    updated

--- summary.py
+++ summary.py
@@ -7,8 +7,6 @@ import altair as alt
 
 def summary(client):
 
-    st.title("SUMMARY")
-
     try:
         # client = get_gsheet_client()
         sheet_id = "1VVLZ0O3NncvMjex8gonkgPTfIKzkJh22UON55991_QE"
