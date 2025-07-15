84035beac7ee993be5a1524a1333f63f81e48c34
Athan <jonpuray@gmail.com>
2025-07-15 14:26:17 +0800

    updated

--- summary.py
+++ summary.py
@@ -53,8 +53,8 @@ def summary(client):
     
     col1, col2 = st.columns(2, border=True)
 
+    st.header('Client Breakdown')
     with col1:  
-        st.header('Client Misses')
         c_list = st.multiselect('Select Client', options=client_list, help='Leave Blank to see all clients')
         button_select = st.button('Select')
 
