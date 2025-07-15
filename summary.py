19d31d3653e5dafb3aa8951ed26181e69100c83d
Athan <jonpuray@gmail.com>
2025-07-15 14:50:18 +0800

    updated

--- summary.py
+++ summary.py
@@ -39,6 +39,9 @@ def summary(client):
     with colb:
         st.header('')
         st.bar_chart(count_month, use_container_width=True)
+        alt.Chart(count_month).mark_bar().encode(
+            order=alt.Order('MONTH_NAME',sort='ascending')
+)
     
     st.header('Daily Breakdown')
     cola1, colb1 = st.columns([0.3, 0.7], border=True)
