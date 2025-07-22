8e7a0f9ad25bf106c216978af574d0d52d3e0533
Athan <jonpuray@gmail.com>
2025-07-22 22:11:39 +0800

    updated

--- summary.py
+++ summary.py
@@ -73,7 +73,7 @@ def summary(client):
     with colb:
         st.header('')        
         st.bar_chart(
-            data=df_monthcount[['MONTH_NAME', 'count']],
+            data=df_monthcount,
             use_container_width=True,
             x_label='Month',
             y_label='Count',
