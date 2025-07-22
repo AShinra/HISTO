1e0f819bda06d5ed576761112d22116a90c2b74b
Athan <jonpuray@gmail.com>
2025-07-22 15:47:58 +0800

    added captured button

--- input.py
+++ input.py
@@ -81,6 +81,7 @@ def input(client, client_list):
                 # get the tiering of the website
                 for k, v in fqdn_dict.items():
                     if k in _hyperlink:
+                        st.write('Found')
                         input_tier = v
 
                 sheet.worksheet('TEMP').append_row([input_date, input_client, input_tier, _hyperlink, captured])
