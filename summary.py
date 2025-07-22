a8a648d9ce0ed646db2b4b8b3d4a523d38ab3f56
Athan <jonpuray@gmail.com>
2025-07-22 21:52:04 +0800

    updated

--- summary.py
+++ summary.py
@@ -65,6 +65,7 @@ def summary(client):
 
         monthcount = df_captured['MONTH_NAME'].value_counts(sort=False)
         df_monthcount = monthcount.to_frame()
+        st.write(df_monthcount.columns)
         st.dataframe(df_monthcount)
 
     with colb:
