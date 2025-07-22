7c23cedd38596615b0bd3018267b1af29a2f9cd9
Athan <jonpuray@gmail.com>
2025-07-22 13:37:01 +0800

    added captured button

--- summary.py
+++ summary.py
@@ -18,7 +18,6 @@ def summary(client):
     with col12:
         st.write('')
         st.write('')
-        st.write('')
         cb_client = st.checkbox(
             label='All Clients',        
         )
