e95fa431f5cf821b75dbe106bc40acce5f5ea8f2
Athan <jonpuray@gmail.com>
2025-07-22 15:48:57 +0800

    added captured button

--- input.py
+++ input.py
@@ -82,7 +82,7 @@ def input(client, client_list):
                 for k, v in fqdn_dict.items():
                     if k in _hyperlink:
                         st.write('Found')
-                        input_tier = v
+                        st.write(k)
 
                 sheet.worksheet('TEMP').append_row([input_date, input_client, input_tier, _hyperlink, captured])
 
