b181dd01a708255d4fc5467705d84952242bd7ba
Athan <jonpuray@gmail.com>
2025-07-26 02:27:35 +0800

    updated

--- summary.py
+++ summary.py
@@ -132,28 +132,12 @@ def summary(client):
                 for _pub in sorted(_misses_tier1_pub):
                     st.write(_pub)
 
-            coltiera1, coltiera2 = st.columns([0.3, 0.7])
-            with coltiera1:
-                with st.popover(
-                    label='Details'
-                ):  
-                    _misses_tier1_pub = list(dict.fromkeys(_misses_tier1_pub))
-                    for _pub in sorted(_misses_tier1_pub):
-                        st.write(_pub)
-            with coltiera2:
-                st.write(f'Tier 1 Missed: {count_misses_tier1}')
-            
-
-            coltierb1, coltierb2 = st.columns([0.3, 0.7])
-            with coltierb1:                            
-                with st.popover(
-                    label='Details'
-                ):  
-                    _misses_tier2_pub = list(dict.fromkeys(_misses_tier2_pub))
-                    for _pub in sorted(_misses_tier2_pub):
-                        st.write(_pub)
-            with coltierb2:
-                st.write(f'Tier 2 Missed: {count_misses_tier2}')
+            with st.expander(
+                label=f'Tier 2 Missed: {count_misses_tier2}'
+            ):
+                _misses_tier2_pub = list(dict.fromkeys(_misses_tier2_pub))
+                for _pub in sorted(_misses_tier2_pub):
+                    st.write(_pub)
             
             coltierc1, coltierc2 = st.columns([0.3, 0.7])
             with coltierc1:
