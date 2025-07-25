b11f7bd31c1b8395fea05b9732422f9be5b586cb
Athan <jonpuray@gmail.com>
2025-07-26 02:13:06 +0800

    updated

--- summary.py
+++ summary.py
@@ -121,7 +121,7 @@ def summary(client):
             <p span style="color:red;"><b>Total Misses: </b>{int(total_misses):,} ({misses_percent:.2%})</p>
             <p style="color:red;"><b>Avg Misses/Month: </b>{int(misses_per_month):,}</p>
             <p style="color:red;"><b>Avg Misses/Day: </b>{int(misses_per_day):,}</p>
-            <p></p>
+            <p>"ssss"</p>
             </div>
             """, unsafe_allow_html=True)
 
