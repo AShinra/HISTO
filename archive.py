7fd45148acb216fae0b4f783e4599be15f4acf4b
Athan <jonpuray@gmail.com>
2025-07-25 10:12:48 +0800

    updated

--- archive.py
+++ archive.py
@@ -44,18 +44,22 @@ def archive(client):
                 horizontal=False)
             
         with col2:
-            today = date.today()
-            start_date = today
-            end_date = today
 
-            _date = st.date_input(
-                label='DATE',
-                key='a_date',
-                value=(start_date, end_date),
-                min_value=date(2025, 1, 1),
-                max_value=date(2030, 12, 31))
-            
-            st_date, en_date = _date
+            try:
+                today = date.today()
+                start_date = today
+                end_date = today
+
+                _date = st.date_input(
+                    label='DATE',
+                    key='a_date',
+                    value=(start_date, end_date),
+                    min_value=date(2025, 1, 1),
+                    max_value=date(2030, 12, 31))
+                
+                st_date, en_date = _date
+            except:
+                st.write("Please select a valid date range.")
     
         with col3:
             _client = st.multiselect('CLIENT', key='a_client', options=client_list)
@@ -82,9 +86,7 @@ def archive(client):
 
                             st_date = st_date.isoformat()
                             en_date = en_date.isoformat()
-                            # formatted_date_1 = datetime.strptime(_date, '%Y-%m-%d')
-                            # formatted_date_1 = formatted_date_1.strftime('%-m/%-d/%Y')
-                            
+                                                        
                             st.header(f':violet[{cl}]')
 
                             col_off1, col_off2 = st.columns(2, border=True)
@@ -103,21 +105,7 @@ def archive(client):
 
                                 st.subheader(f':red[Missed - {sel_off_missed.shape[0]}]')
                                 st.dataframe(sel_off_missed, hide_index=True, use_container_width=True)
-
-
-
-                        # if captured_options == 'Captured':
-                        #     filtered_df = df[(df['DATE'] == formatted_date_1) & (df['CLIENT NAME'] == cl) & (df['CAPTURED'] == 'Y')]
-                        # elif captured_options == 'Missed':
-                        #     filtered_df = df[(df['DATE'] == formatted_date_1) & (df['CLIENT NAME'] == cl) & (df['CAPTURED'] == 'N')]
-
-                        # st.write(formatted_date_1)
-                        # if filtered_df.shape[0] > 0:
-                        #     selected_columns = filtered_df[['DATE', 'TIER', 'LINK']]
-                        #     st.header(f'{cl} {captured_options} - {selected_columns.shape[0]}')
-                        #     st.dataframe(selected_columns, use_container_width=True, hide_index=True)
-                        # elif filtered_df.shape[0] == 0:
-                        #     st.error('No Data Found')
+                        
 
             elif radio_options == 'All Clients':
 
